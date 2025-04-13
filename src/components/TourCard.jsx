d3523a9b6cc30151fe0689fb9b687a6d483c2262
ashlym1 <ashlym3@usf.edu>
2025-04-13 17:39:05 -0400

    Build TourCard with conditional rendering and interaction

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-// BookCard renders individual book details
+// Tourcard displays individual tour info 
 const BookCard = ({ id,name , price , info, onRemove }) => {
   // Local state to toggle "Read More" / "Show Less"
   const [readMore, setReadMore] = useState(false);
@@ -9,21 +9,24 @@ const BookCard = ({ id,name , price , info, onRemove }) => {
   const safeSummary = info || "No information available.";
 
   return (
-    <article className="book-card">
-      <h3>{name}</h3>
-      <h5>{price}</h5>
+    <article className="tour-card">
+      <img src={image} alt={name} />
+      <div className="tour-header">
+        <h3>{name}</h3>
+        <h5>${price}</h5>
+      </div>
       <p>
-        {readMore ? safeSummary : `${safeSummary.substring(0, 80)}...`}
+        {readMore ? shortInfo : `${shortInfo.substring(0, 100)}... `}
         <button onClick={() => setReadMore(!readMore)}>
           {readMore ? "Show Less" : "Read More"}
         </button>
       </p>
       {/* Button to remove the book */}
       <button className="btn-remove" onClick={() => onRemove(id)}>
-        Remove Book
+       Not Interested 
       </button>
     </article>
   );
 };
 
-export default BookCard;
+export default TourCard;
